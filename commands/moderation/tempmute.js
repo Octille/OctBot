ff815f2ea0913c0147e44cd84035d5fbc16edb97
Gurkirat <sainigurkirat111@gmail.com>
2021-03-26 15:33:25 -0500

    Temp mute

--- commands/moderation/tempmute.js
+++ commands/moderation/tempmute.js
@@ -1,3 +1,4 @@
+const ms = require('ms');
 module.exports = {
     name: 'tempmute',
     description: 'tempmutes a member',
@@ -5,24 +6,40 @@ module.exports = {
         if(message.member.hasPermission('MANAGE_MESSAGES')) {
             var member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
             if(!member) return message.reply('Please Provide a Member to TempMute.')
-
-            let mainrole = message.guild.roles.cache.find(role => role.name === "Nons");
-            let role = message.guild.roles.cache.find(role => role.name === "Muted");
-
-            if (!role) return message.reply("Couldn't find the 'muted' role, create a role name 'Muted' and disable send message feature")
+            let role = message.guild.roles.cache.find(role => role.name === "muted");
+            
+            if(!role) {
+                try {
+                    message.channel.send('Muted role is not found, attempting to create muted role.')
+    
+                    let muterole = await message.guild.roles.create({
+                        data : {
+                            name : 'muted',
+                            permissions: []
+                        }
+                    });
+                    message.guild.channels.cache.filter(c => c.type === 'text').forEach(async (channel, id) => {
+                        await channel.createOverwrite(muterole, {
+                            SEND_MESSAGES: false,
+                            ADD_REACTIONS: false
+                        })
+                    });
+                    message.channel.send('Muted role has sucessfully been created.')
+                    role = muterole
+                } catch (error) {
+                    console.log(error)
+                }
+            };
 
             let time = args[1];
             if (!time) {
                 return message.reply("You didnt specify a time!");
             }
-
-            member.roles.remove(mainrole.id)
             member.roles.add(role.id);
 
             message.channel.send(`@${member.user.tag} has now been muted for ${ms(ms(time))}`)
 
             setTimeout( function () {
-                member.roles.add(mainrole.id)
                 member.roles.remove(role.id);
                 message.channel.send(`@${member.user.tag} has been unmuted.`)
             }, ms(time));
