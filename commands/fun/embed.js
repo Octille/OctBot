267c36cdbc443131dc771890990265e9deed7696
Gurkirat <sainigurkirat111@gmail.com>
2021-03-25 20:25:41 -0500

    Update embed.js

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -7,20 +7,25 @@ module.exports = {
     async execute(message,args, cmd, client, Discord) {
         let title = 0
         let description = 0
-        message.channel.send('please provid a title')
+        
         const filter = (m) => m.author.id === message.author.id
         const collector = message.channel.createMessageCollector(filter, { max: 2, time: 15000 });
-        
+        message.channel.send('please provid a title').then(() =>{})
+        let deleted = 0;
         collector.on('collect', m => {
+            
             if(title == '0'){
                 const deltetitle = message.id
               
                 title = m.content
-                return message.channel.send(`Your title has been set to \`${m.content}\`, now please provide a description`)
+                m.delete();
+                return message.channel.send(`Your title has been set to \`${m.content}\`, now please provide a description`).then( msgg =>{ 
+                 })
             }
             if(description == '0'){
                 const deletedescription = message.id
                 description = m.content
+                m.delete();
             }
             const creatingembed = new Discord.MessageEmbed()
             .setTitle(`creating embed...`);
@@ -32,13 +37,16 @@ module.exports = {
                 .setDescription(description)
                 .setFooter(`Embed created by ${message.author.username}`)
               setTimeout(() => {
-                msg.edit(embed);
+                msg.delete();
+                message.channel.send(embed)
+
       
 
                 
               
               }, 1000);
             })
+
 
 
             
