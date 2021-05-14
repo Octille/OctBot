39128519bdf0b4872b492d8e0a8df76dfa1681ad
Gurkirat <sainigurkirat111@gmail.com>
2021-05-14 09:44:07 -0500

    Update invite.js

--- commands/misc/invite.js
+++ commands/misc/invite.js
@@ -7,7 +7,7 @@ module.exports = {
         const invite = new Discord.MessageEmbed()
         .setDescription(`Hi im <@741776473613926490> and heres all my invite links`, `_ _`)
         .addField(`Invite Oct:`, `[click me](https://discord.com/oauth2/authorize?client_id=741776473613926490&scope=bot&permissions=5166394750)`, true)
-        .addField(`Oct server:`, `[click me](https://discord.gg/hE28auh4R5)`, true)
+        .addField(`Oct server:`, `[click me](https://discord.gg/RJBeFnzbRe)`, true)
         .addField(`top.gg link:`, `[click me](https://top.gg/bot/432610292342587392/invite/)`, true)
         .addField(`Oct Bot Website:`, `[click me](https://octbot.ml/home/)`)
 
