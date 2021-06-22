10a71ed61ac5ff2268a4f396c5cd602d98e24705
Gurkirat <sainigurkirat111@gmail.com>
2021-06-22 09:52:37 -0500

    Update autorole.js

--- commands/moderation/autorole.js
+++ commands/moderation/autorole.js
@@ -39,7 +39,7 @@ module.exports = {
             
         })
     } else {
-        await data.findOneAndDelete({
+        await data.findOneAndRemove({
             GuildID: message.guild.id
         });
         message.channel.send(`**Successfuly Reset the auto role on your Server!**\nplease use this command again to re-setup!`);
