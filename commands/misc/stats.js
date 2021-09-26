050a0bd68fdd6e21de5570a3faba9f5b44bf670b
Gurkirat <sainigurkirat111@gmail.com>
2021-09-26 16:11:45 -0500

    Update stats.js

--- commands/misc/stats.js
+++ commands/misc/stats.js
@@ -2,6 +2,7 @@ const ms = require('ms');
 module.exports = {
     name: 'stats',
     async execute(message,args, cmd, client, Discord, profileData){
+        const nextRestart = ms(86400000 - client.uptime, {long: true})
         const statsEmbed = new Discord.MessageEmbed()
         .setColor("BLUE")
         .setTitle('My Stats')
@@ -9,7 +10,9 @@ module.exports = {
         .addField(`Members:`,`\`${client.users.cache.size}\``, true)
         .addField(`Commands:`,`\`${client.commands.size}\``, true)
         .addField(`Uptime:`,`\`${ms(client.uptime, { long: true }) }\``, true)
-        .addField(`Online since:`,`\`${client.readyAt.toDateString()}\``, true)
+        .addField(`Estimated Restart In:`,`\`${nextRestart}\``, true)
+        .addField(`Online Since:`,`\`${client.readyAt.toDateString()}\``, true)
+        .setFooter(`Oct bot restarts every 24 hour to prevent lag.\nEstimated downtime is 10-15s`)
         message.channel.send(statsEmbed)
  
     }
